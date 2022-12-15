2e90785f219962860485f3ce6449c548e4afa0db
michalAndrzejewski <pl.michal.and@gmail.com>
2022-12-15 16:53:02 +0100

    Another commit

--- job_board_api/core/admin.py
+++ job_board_api/core/admin.py
@@ -1,17 +1,47 @@
 """
-Django admin customization
+Django admin customization.
 """
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
 
 class UserAdmin(BaseUserAdmin):
-    """Define the admin pages for users"""
+    """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (_('Credentials'), {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_superuser',
+                    'is_staff',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 
 admin.site.register(User, UserAdmin)
---
+++ job_board_api/readme.md
@@ -0,0 +1,3 @@
+## This is a Django REST Framework based application.
+## It was created to store, manage, and view available job listings in the database.
+#### (It's still in progress)
